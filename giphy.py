c2d3c7d2a9911d915b9829e64a208e9b9abdf78f
lorena <lorenagb16@gmail.com>
2021-06-14 20:22:12 +0200

    giphy

--- giphy.py
+++ giphy.py
@@ -18,8 +18,8 @@ Giphy_api = os.getenv("GIPHY_API")
 bot = commands.Bot(command_prefix="!")
 
 @bot.command()
-async def random(ctx):
-    ghiphy_url =f'https://api.giphy.com/v1/gifs/random?api_key={Giphy_api}&tag=&rating=g'
+async def random(ctx, tag : str = ""):
+    ghiphy_url =f'https://api.giphy.com/v1/gifs/random?api_key={Giphy_api}&tag={tag}&rating=g'
     async with aiohttp.ClientSession() as session:
         async with session.get(ghiphy_url) as response:
             if response.status == 200:
@@ -27,14 +27,6 @@ async def random(ctx):
                 await ctx.send (js ["data"]["images"]["original"]["url"])
     return
 
-@bot.command()
-async def search(ctx,search_term):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(chucknorris_search_api+search_term) as response:
-            if response.status == 200:
-                js = await response.json()
-                await ctx.send (rnd.choice(js["result"])["value"])
-    return
 
 
 bot.run(TOKEN)
